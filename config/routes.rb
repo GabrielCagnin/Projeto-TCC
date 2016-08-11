ab095c5f038bc0c32e327f7e1c769f33b59afb03
Adriano Dennanni <adrianodennanni@gmail.com>
2016-08-11 00:44:18 -0300

    Remove default routes from AcquisitionSets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :acquisition_sets
   devise_for :users, defaults: {format: :json}
 
   post '/create_facility', to: 'facilities#create_facility', defaults: {format: :json}
@@ -8,6 +7,7 @@ Rails.application.routes.draw do
 
   post '/create_zone', to: 'zones#create_zone', defaults: {format: :json}
   get '/zones', to: 'zones#show_all', defaults: {format: :json}
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
