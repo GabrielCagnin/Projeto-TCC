e444971bcc74d4ac0abe7e0df86d99a6920082d4
Adriano Dennanni <adrianodennanni@gmail.com>
2016-09-26 23:39:15 -0300

    Test 3

--- app/controllers/classify_zone_controller.rb
+++ app/controllers/classify_zone_controller.rb
@@ -4,7 +4,7 @@ class ClassifyZoneController < ApplicationController
   def get_zone
     con=Rserve::Connection.new
 
-    x=con.eval('PrepareData(57b6043d19e3c17d5ac586ef)')
+    x=con.eval('aws.PrepareData("57b6043d19e3c17d5ac586ef")')
     render json: x
   end
 end
