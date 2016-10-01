5e22baed80be65f8a09455e5e5fc9e49f1b86ce7
Adriano Dennanni <adrianodennanni@gmail.com>
2016-10-01 15:28:23 -0300

    Removed rserve from initializer

--- app/controllers/classify_zone_controller.rb
+++ app/controllers/classify_zone_controller.rb
@@ -3,11 +3,22 @@ class ClassifyZoneController < ApplicationController
 
   def get_zone
 
+    require 'rserve'
+
+# Connect to Rserve
+    con=Rserve::Connection.new
+
+# Change R to point to correct dir
+    con.eval('setwd("~/machinelearning_R")')
+
+# Source all functions in Rserve workspace
+    con.eval('source("serverFunctions.r")')
+
 
     #first, prepare data with correct facility ID
-    x=Rserve_con.eval('aws.PrepareData("57b6043d19e3c17d5ac586ef")')
+    x=con.eval('aws.PrepareData("57b6043d19e3c17d5ac586ef")')
     #then, we train the models
-    y=Rserve_con.eval('aws.trainModels("57b6043d19e3c17d5ac586ef")')
+    y=con.eval('aws.trainModels("57b6043d19e3c17d5ac586ef")')
 
     render json: x
 
