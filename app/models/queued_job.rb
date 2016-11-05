021950dc49da13f0c553df6d7498a58a200d73f5
Adriano Dennanni <adrianodennanni@gmail.com>
2016-11-05 11:57:14 -0200

    Removed queued class

--- app/models/queued_job.rb
+++
@@ -1,9 +0,0 @@
-class QueuedJob
-  include Mongoid::Document
-
-  embeds_many :access_points
-
-  field :facility_id, type: String
-  field :op_type, type: String
-  field :response, type: String
-end
